ce28e53104b26157ab46bf6ef6d13e3b76181d53
venkyps <venkatcs85@gmailcom>
2025-06-26 23:17:51 +0800

    Shift string value and check they are equal

---
+++ src/main/java/com/spring/learning/SpringBootLearning/javacodility/RotatingStringValue.java
@@ -0,0 +1,21 @@
+package com.spring.learning.SpringBootLearning.javacodility;
+
+public class RotatingStringValue {
+
+    public static int solution(String value) {
+        int N = value.length();
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            char firstChar = value.charAt(i);
+            char lastChar = value.charAt((i+N-1)%N);
+            if (firstChar == lastChar) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("value of the string rotation " + solution("abab"));
+    }
+}
