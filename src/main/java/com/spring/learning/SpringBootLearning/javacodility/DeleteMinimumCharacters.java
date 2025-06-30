9ce27d3d8164e4ab3722e15a526c6981b2360fee
venkyps <venkatcs85@gmailcom>
2025-06-30 23:23:16 +0800

    Delete minimum character occurence

---
+++ src/main/java/com/spring/learning/SpringBootLearning/javacodility/DeleteMinimumCharacters.java
@@ -0,0 +1,31 @@
+package com.spring.learning.SpringBootLearning.javacodility;
+
+public class DeleteMinimumCharacters {
+
+    public static int minimumCharacterDeletion(String value) {
+        int countA = 0;
+        int countB = 0;
+        int minDeletion = 0;
+
+        for (char c : value.toCharArray()) {
+            if ('A' == c) {
+                countA++;
+            }
+        }
+        minDeletion = countA;
+
+        for (char c : value.toCharArray()) {
+            if ('B' == c) {
+                countB++;
+            } else {
+                countA--;
+            }
+            minDeletion = Math.min(minDeletion, countA + countB);
+        }
+        return minDeletion;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("value of the deletion  "+minimumCharacterDeletion("BAAABAB"));
+    }
+}
